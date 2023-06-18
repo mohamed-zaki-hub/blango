71a3f8d6a8ee01e148f773fd56ba368ce6dd457f
mohamed hosny <mohamed_zaki221@yahoo.com>
2023-06-18 14:03:50 +0000

    Finish security ad passwords

--- blango/settings.py
+++ blango/settings.py
@@ -146,8 +146,12 @@ class Dev(Configuration):
             "level": "DEBUG",
         },
     }
-
-
+    PASSWORD_HASHERS = [
+        'django.contrib.auth.hashers.Argon2PasswordHasher',
+        'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+        'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+        'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    ]
 
     # Internationalization
     # https://docs.djangoproject.com/en/3.2/topics/i18n/
