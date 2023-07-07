9a0612979f6e959ffddddc85be2cc07f5ea015a3
mohamed hosny <mohamed_zaki221@yahoo.com>
2023-07-07 22:33:04 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -50,12 +50,22 @@ class Dev(Configuration):
         'django.contrib.sessions',
         'django.contrib.messages',
         'django.contrib.staticfiles',
+        'django.contrib.sites',
         'blog',
         'crispy_forms',
         'crispy_bootstrap5',
         'debug_toolbar',
         'blango_auth',
+        'allauth',
+        'allauth.account',
+        'allauth.socialaccount',
+        'allauth.socialaccount.providers.google',
     ]
+    SITE_ID = 1
+    ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_USERNAME_REQUIRED = False
+    ACCOUNT_AUTHENTICATION_METHOD = "email"
     AUTH_USER_MODEL ="blango_auth.User"
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
     ACCOUNT_ACTIVATION_DAYS = 7
